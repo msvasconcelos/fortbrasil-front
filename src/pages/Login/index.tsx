c39d8daf9007187ab9698f051fbc1ac112c27268
Matheus Vasconcelos <matheus-s.v@hotmail.com>
2020-08-30 09:59:30 -0300

    create pages login,register, components, service

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,37 @@
+/* eslint-disable arrow-body-style */
 import React from 'react';
+import { FiArrowRight, FiMail, FiLock } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
-const Login: React.FC = () => <h1>Página Login</h1>;
+import { Container, Content } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+const Login: React.FC = () => {
+  return (
+    <Container>
+      <Content>
+        <form>
+          <h1>Faça seu logon</h1>
+
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Entrar</Button>
+        </form>
+
+        <Link to="/register">
+          <FiArrowRight size={20} />
+          Criar conta
+        </Link>
+      </Content>
+    </Container>
+  );
+};
 
 export default Login;
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,5 +1,40 @@
+/* eslint-disable arrow-body-style */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Register: React.FC = () => <h1>Página Register</h1>;
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content } from './styles';
+
+const Register: React.FC = () => {
+  return (
+    <Container>
+      <Content>
+        <form>
+          <h1>Faça seu cadastro</h1>
+
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </form>
+
+        <Link to="/">
+          <FiArrowLeft size={20} />
+          Voltar para logon
+        </Link>
+      </Content>
+    </Container>
+  );
+};
 
 export default Register;
